ab230715a7805ffab401a536a7933da1cb77f0a0
iioan <teo.ioan12.ti@gmail.com>
2025-11-01 21:16:20 +0200

    Add Navbar component and Tailwind CSS configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
-import { Heading, Text } from "@radix-ui/themes";
+import {Heading, Text} from "@radix-ui/themes";
+import {Navbar} from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="container mx-auto max-w-6xl px-4 py-14">
-      {/* Hero */}
-      <section className="mb-10">
-        <Heading size="8" className="mb-3 tracking-tight">
-          EGOV Lab Application
-        </Heading>
-        <Text size="4" className="text-slate-600">
-          A Next.js + TypeScript app with both frontend UI and API routes. Radix UI
-          primitives + Tailwind for styling.
-        </Text>
-      </section>
-    </main>
+    <>
+      <Navbar/>
+      <main className="container mx-auto max-w-6xl px-4 py-14">
+        {/* Hero */}
+        <section className="mb-10">
+          <Heading size="8" className="mb-3 tracking-tight">
+            EGOV Lab Application
+          </Heading>
+          <Text size="4" className="text-slate-600">
+            A Next.js + TypeScript app with both frontend UI and API routes. Radix UI
+            primitives + Tailwind for styling.
+          </Text>
+        </section>
+      </main>
+    </>
   );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Image from "next/image";
+import {Heading} from "@radix-ui/themes";
+
+export const Navbar = () => {
+  return (
+    <nav
+      aria-label="Main Navigation"
+      className="sm:h-20 px-12 flex items-center justify-between bg-black/10"
+    >
+      <a href="#" title="My product">
+        <Image src="/upb_logo.png"
+               width={80}
+               height={80}
+               alt="Picture of the author"/>
+      </a>
+      <div>
+        <Heading>
+          Plată taxe universitate
+        </Heading>
+      </div>
+      <div className="gap-3 items-center hidden md:flex">
+        <a
+          href="#"
+          className="inline-flex px-5 py-2 blink-text-primary hover:bg-blinkGray400 dark:hover:bg-blinkGray800 rounded-full"
+        >
+          Formular
+        </a>
+        <a
+          href="#"
+          className="inline-flex px-5 py-2 blink-text-primary bg-blinkGray400 hover:bg-blinkGray500 dark:bg-blinkGray800 dark:hover:bg-blinkGray700 rounded-full"
+        >
+          Plati
+        </a>
+      </div>
+    </nav>
+  );
+};
